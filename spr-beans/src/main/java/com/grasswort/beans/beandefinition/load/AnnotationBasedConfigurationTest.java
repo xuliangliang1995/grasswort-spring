406190a2bffa0aa06b8a6d3298d05bdfe46fad4f
xuliang <grasswort@qq.com>
2020-09-15 13:22:36 +0800

    AutowiredAnnotationBeanPostProcessor

--- spr-beans/src/main/java/com/grasswort/beans/beandefinition/load/AnnotationBasedConfigurationTest.java
+++ spr-beans/src/main/java/com/grasswort/beans/beandefinition/load/AnnotationBasedConfigurationTest.java
@@ -4,7 +4,13 @@ import com.grasswort.beans.model.User;
 import com.grasswort.beans.model.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessorAdapter;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -22,21 +28,47 @@ public class AnnotationBasedConfigurationTest {
     private static Logger logger = LoggerFactory.getLogger(AnnotationBasedConfigurationTest.class);
 
     public static void main(String[] args) {
-        // DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        GenericApplicationContext beanFactory = new GenericApplicationContext();
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        //GenericApplicationContext beanFactory = new GenericApplicationContext();
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
 
         int loadCount = xmlBeanDefinitionReader.loadBeanDefinitions("com/grasswort/beans/beandefinition/configurationmeta/user-service-annotation-based.xml");
         logger.info("已解析 BeanDefinition 数量 : {}", loadCount);
 
+        AutowiredAnnotationBeanPostProcessor beanPostProcessor = new AutowiredAnnotationBeanPostProcessor();
+        beanPostProcessor.setBeanFactory(beanFactory);
+        beanFactory.addBeanPostProcessor(beanPostProcessor);
+        beanFactory.addBeanPostProcessor(new PostPropertiesBeanPostProcessor());
+
         Stream.of(beanFactory.getBeanDefinitionNames())
                 .forEach(System.out::println);
 
-        beanFactory.refresh();
+        User tom = (User) beanFactory.getBean("tom");
+        System.out.println(tom);
 
         UserService userService = beanFactory.getBean(UserService.class);
         beanFactory.getBeansOfType(User.class).values().forEach(userService::addUser);
         userService.listUser().forEach(System.out::println);
+    }
+
+    public static class PostPropertiesBeanPostProcessor extends InstantiationAwareBeanPostProcessorAdapter {
+        @Override
+        public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+            if ("tom".equals(beanName)) {
+                MutablePropertyValues tomPropertyValues = new MutablePropertyValues();
+                tomPropertyValues.addPropertyValue("id", 1L);
+                tomPropertyValues.addPropertyValue("name", "tom");
+                tomPropertyValues.addPropertyValue("age", 18);
+                return tomPropertyValues;
+            } else if ("jerry".equals(beanName)) {
+                MutablePropertyValues jerryPropertyValues = new MutablePropertyValues();
+                jerryPropertyValues.addPropertyValue("id", 2L);
+                jerryPropertyValues.addPropertyValue("name", "jerry");
+                jerryPropertyValues.addPropertyValue("age", 8);
+                return jerryPropertyValues;
+            }
+            return pvs;
+        }
     }
 
 }
