c2c26c640b6f0282e80ac99fa9fba4d4dc0752c7
许良良 <grasswort@qq.com>
2020-09-19 08:31:19 +0800

    circularRef

---
+++ spr-beans/src/main/java/com/grasswort/beans/circular/CircularConfiguration.java
@@ -0,0 +1,14 @@
+package com.grasswort.beans.circular;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xuliangliang
+ * @Description
+ * @Date 2020/9/17
+ */
+@Configuration
+@ComponentScan("com.grasswort.beans.circular")
+public class CircularConfiguration {
+}
