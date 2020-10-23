38e82715f48d13119a9a5a14e8776bf9eafbb483
许良良 <grasswort@qq.com>
2020-10-23 15:40:13 +0800

    aop test

---
+++ spr-beans/src/main/java/com/grasswort/beans/aop/PessimisticLockingFailureException.java
@@ -0,0 +1,9 @@
+package com.grasswort.beans.aop;
+
+/**
+ * @author xuliangliang
+ * @Description
+ * @Date 2020/10/23
+ */
+public class PessimisticLockingFailureException extends RuntimeException {
+}
